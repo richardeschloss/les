167f3c406ff513f1f9a3e2ac3185eeae83b6addc
Richard Schloss <richardeschloss@gmail.com>
2019-11-15 16:33:06 -0700

    Fixed minor bug. Including more bin files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,9 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  rules: {}
+  rules: {
+    "comma-dangle": ["error", "never"],
+    "prefer-const": ["error"],
+    "no-var": ["error"]
+  }
 }
